8ee9e8286f29ab83a0b0e7a9ca26dae224df170d
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-06-08 13:02:35 +0300

    copies monday into tuesday,wednesday,thursday,friday

--- monday/assets/js/script.js
+++ monday/assets/js/script.js
@@ -25,3 +25,32 @@ let displayFooterContent = () =>{
 	"Copyright &copy; "+currentYear+". All Rights Reserved.</center>";
 	document.getElementById(projectFooter).innerHTML+=content;
 }
+
+/*******************************************
+
+Make an array of numbers, then use .map() to return 
+
+an array containing each of the original numbers + 1
+	
+******************************************************/
+let numbers = [1,2,3,4,5];
+let incrementedNumbers= numbers.map((number)=>{
+	return number+1;
+});
+
+console.log(incrementedNumbers);
+
+/*******************************************
+
+Create an array of words. Use .map() to return
+
+an array of the same words in all uppercase.
+	
+******************************************************/
+let words=['dogs','favorite','yellow','mangoes','reliability','confession'];
+
+let uppercaseWords = words.map((word)=>{
+	return word.toUpperCase();
+});
+
+console.log(uppercaseWords);
