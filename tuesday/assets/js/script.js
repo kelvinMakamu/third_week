ed92db008aed3eb998f0237f3b9c07167a54c6aa
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-06-09 15:50:48 +0300

    Adds jquery JS library

--- tuesday/assets/js/script.js
+++ tuesday/assets/js/script.js
@@ -21,36 +21,7 @@ let currentYear=today.getFullYear();
 
 ***************/
 let displayFooterContent = () =>{
-	let content="<center><span>jQuery Creating interacive web pages</span><br/><br/>"+
+	let content="<center><span>Creating interactive web pages</span><br/><br/>"+
 	"Copyright &copy; "+currentYear+". All Rights Reserved.</center>";
 	document.getElementById(projectFooter).innerHTML+=content;
 }
-
-/*******************************************
-
-Make an array of numbers, then use .map() to return 
-
-an array containing each of the original numbers + 1
-	
-******************************************************/
-let numbers = [1,2,3,4,5];
-let incrementedNumbers= numbers.map((number)=>{
-	return number+1;
-});
-
-console.log(incrementedNumbers);
-
-/*******************************************
-
-Create an array of words. Use .map() to return
-
-an array of the same words in all uppercase.
-	
-******************************************************/
-let words=['dogs','favorite','yellow','mangoes','reliability','confession'];
-
-let uppercaseWords = words.map((word)=>{
-	return word.toUpperCase();
-});
-
-console.log(uppercaseWords);
