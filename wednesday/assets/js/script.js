9cb8fefa7d437ab904a9bec1f483ed0ca2db0df1
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-06-11 12:40:23 +0300

    Refactoring MadLibs code to enforce DRY

--- wednesday/assets/js/script.js
+++ wednesday/assets/js/script.js
@@ -114,14 +114,11 @@ let toggleTermsDefinition = (conceptID,definitionID) =>{
 
 $(document).ready(()=> {
     $("#blanks form").submit((event) => {
-        $(".person1").text($("input#person1").val());
-        $(".person2").text($("input#person2").val());
-        $(".animal").text($("input#animal").val());
-        $(".exclamation").text($("input#exclamation").val());
-        $(".verb").text($("input#verb").val());
-        $(".noun").text($("input#noun").val());
-
-        $("#story").show();
-        event.preventDefault();
+       event.preventDefault();
+    	let blanks = ['person1','person2','animal','exclamation','verb','noun'];
+    	blanks.forEach((blank)=>{
+        $("."+blank).text($("input#"+blank).val());
+    	});
+      $("#story").show();
     });
 });
